52209ed303825c4b8afec9774e73a23d56c016b6
syj00062 <syj00062@gmail.com>
2022-10-05 21:49:45 +0900

    커밋추가

--- java_demo01/src/kr/co/demo/Demo01.java
+++ java_demo01/src/kr/co/demo/Demo01.java
@@ -2,10 +2,18 @@ package kr.co.demo;
 
 public class Demo01 {
 
+	/**
+	 * 클래스나 메서드에 대한 설명을 작성하기 위한 용도로 사용하지만 블럭주석임
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
-
+		//인라인 주석
+		
+		//출력코드 : 콘솔(터미널0에 문자열 출력
 		System.out.println("Hello World!!");
 		System.out.println("JAVA");
+		//실행코드 마지막에는 반드시 세미콜론 ; 
 	}
 
 }
