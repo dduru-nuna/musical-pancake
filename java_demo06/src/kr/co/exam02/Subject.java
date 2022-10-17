8560ef1e44edadcaf658ca718861ea52d9a08dbb
syj00062 <syj00062@gmail.com>
2022-10-17 21:28:47 +0900

    객체 예제2

---
+++ java_demo06/src/kr/co/exam02/Subject.java
@@ -0,0 +1,56 @@
+package kr.co.exam02;
+
+public class Subject {
+	
+	private String name;
+	private double score;
+	private char grade;
+	
+	public Subject(String name) {
+		this.name = name;     // 자기 자신에 대한 메서드나 변수 사용하면 this 사용
+	}
+	
+	public void setScore(double score) {
+		if(score >= 0 && score <= 100) {
+			this.score = score;
+		} else {
+			this.score = 0;
+		}
+		this.setGrade();
+	}
+	
+	private void setGrade() {
+		switch((int)(this.score /  10)) {
+		case 10:
+		case 9:
+			this.grade = 'A';
+			break;
+		case 8:
+			this.grade = 'B';
+			break;
+		case 7:
+			this.grade = 'C';
+			break;
+		case 6:
+			this.grade = 'D';
+			break;
+		case 5:
+			this.grade = 'E';
+			break;
+		default:
+			this.grade = 'F';
+		}
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getScore() {
+		return this.score;
+	}
+	
+	public char getGrade() {
+		return this.grade;
+	}
+}
