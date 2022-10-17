2f8bea43bb9ed0bb1e03576d160b4339926741bf
syj00062 <syj00062@gmail.com>
2022-10-17 21:46:43 +0900

    예약어

---
+++ java_demo06/src/kr/co/demo05/Demo.java
@@ -0,0 +1,15 @@
+package kr.co.demo05;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		/*
+		 * 필드 예약어
+		 *     final : 한 번만 초기화를 하고 사용할 멤버 필드(변수)에 사용한다(상수)
+		 *     static : 동일한 클래스를 생성된 인스턴스간에 공유할 목적으로 사용하는 
+		 *              필드에 사용을 한다.
+		 */
+
+	}
+
+}
