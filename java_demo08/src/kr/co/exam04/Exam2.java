c70d0d0ec80ac47ecf759fab7cec0ae6dc886278
syj00062 <syj00062@gmail.com>
2022-10-27 21:47:35 +0900

    날짜 클래스 예제 (강사님 풀이)

--- java_demo08/src/kr/co/exam04/Exam2.java
+++ java_demo08/src/kr/co/exam04/Exam2.java
@@ -1,6 +1,8 @@
 package kr.co.exam04;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Exam2 {
@@ -8,28 +10,24 @@ public class Exam2 {
 	public static void main(String[] args) {
 
 		GregorianCalendar gc = new GregorianCalendar(2022,9,1);
-		GregorianCalendar gc2 = new GregorianCalendar(2022,10,0); // 10월의 마지막날
-		int lastDate = gc2.get(Calendar.DATE);
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd E");
+	//	GregorianCalendar gc2 = new GregorianCalendar(2022,10,0); // 10월의 마지막날
+	//	int lastDate = gc2.get(Calendar.DATE);
+		int lastDate = gc.getMaximum(Calendar.DAY_OF_MONTH); //getMaximum 메서드로 그 달(DAY_OF_MONTH)의 마지막일을 알 수 있다
+		
+		Date[] dateArr = new Date[0]; //문자열 배열 말고 Date 배열에 저장하기
 		
 		for(int i = 1 ; i <= lastDate; i++) {
-			int year = gc.get(Calendar.YEAR);
-			int month = gc.get(Calendar.MONTH) + 1;
-			int date = gc.get(Calendar.DATE);
 			int day = gc.get(Calendar.DAY_OF_WEEK);
-			String sDay = "";
-			if(month == 11) {
-				break;
-			}
+	
 			switch(day) {
 			case Calendar.SUNDAY:
-				sDay = "일요일";
-				System.out.println(year + "-" + month + "-" + date + " " + sDay);
+				System.out.println(df.format(gc.getTime()));
 				break;
 			case Calendar.SATURDAY:
-				sDay = "토요일";
-				System.out.println(year + "-" + month + "-" + date + " " + sDay);
+				System.out.println(df.format(gc.getTime()));
 			}
-			gc = new GregorianCalendar(2022, 9, i+1);
+			gc.add(Calendar.DATE, 1);
 		}
 	}
 
