3665b32d878174b5cadc2a65fe65a3ca720d5e7e
syj00062 <syj00062@gmail.com>
2022-12-27 21:46:34 +0900

    run09 다시 푸는 중

--- static_web01/webapp/static/js/sample09.js
+++ static_web01/webapp/static/js/sample09.js
@@ -13,8 +13,27 @@
 }, false);*/
 
 window.onload = function() {
-    var form = document.forms[0];
-    form.username.addEventListener("blur", function(e) { lengthValid(e, 5, 20) });
+    form = document.forms[0];
+    form.username.addEventListener("blur", function(e) { requiredValid(e); })
+    form.username.addEventListener("change", function(e) { lengthValid(e, 5, 20, "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.") });
+    form.username.addEventListener("change", function(e) { charValid(e, "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.", lower=true, upper=false, number=true, special=true, specialChar="_-"); });
+
+    form.pass1.addEventListener("blur", function(e) { requiredValid(e); });
+    form.pass1.addEventListener("change", function(e) { lengthValid(e, 8, 16, "8~16자의 영문 대소문자, 숫자와 특수문자를 사용하세요.") });
+    form.pass1.addEventListener("change", function(e) { charValid(e, "8~16자의 영문 대소문자, 숫자와 특수문자를 사용하세요.", lower=true, upper=true, number=true, special=true); });
+
+    form.pass2.addEventListener("blur", function(e) { requiredValid(e); });
+    form.pass2.addEventListener("change", function(e) { lengthValid(e, 8, 16, "8~16자의 영문 대소문자, 숫자와 특수문자를 사용하세요."); });
+    form.pass2.addEventListener("change", function(e) { equalValue(e, form.pass1); });
+
+    createMonthOption();
+}
+
+function requiredValid(event) {
+    var inputElement = event.target;
+    if(inputElement.value = ""){
+        errorMessage(inputElement, "필수 입력입니다.")
+    }
 }
 
 function lengthValid(event, min, max) {
@@ -22,4 +41,12 @@ function lengthValid(event, min, max) {
     if(inputElement.value.length >= min && inputElement.value.length <= max) {
         form.classList.add('was-validation')
     }
+}
+
+function errorMessage(element, message) {  //event.target인 inputElement를 element로 받아옴
+    var divError = document.createElement("div"); //에러메시지 출력할 div 생성
+    if(element.parentElement.lastElementChild.classList.contains("invalid-feedback") ||
+       element.parentElement.lastElementChild.classList.contains("valid-feedback")) {
+        divError = element.parentElement.lastElementChild; //피드백 메시지가 이미 있는 경우 출력 메시지 위치 다시 조정
+       }
 }
