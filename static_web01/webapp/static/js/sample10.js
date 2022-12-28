6e7a95e016d6455f437535abbdfe556a3b685c7d
syj00062 <syj00062@gmail.com>
2022-12-28 21:46:58 +0900

    JQuery 사용 예시 풀이중

--- static_web01/webapp/static/js/sample10.js
+++ static_web01/webapp/static/js/sample10.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){         // window.onload = funcion() { } 이거랑 같은 역할
+$(document).ready(function() {         // window.onload = function() { } 이거랑 같은 역할
     $("form button").on("click", function(e) { addBookmark(e.target.form); });
+    $(".up").on("click", function(e) { up(e); });
+    $(".down").on("click", function(e) { down(e); });
 });
 
 
@@ -10,3 +12,19 @@ function addBookmark(form) {   //this.form : DOM -> jquery 로 변환 필요
     $(".bookmark-list").append(liTag);
 }
 
+
+function up(event) {
+    var li = $(event).closest('li');
+    var prev = li.prev();
+    if(prev.length) {
+        li.detach().insertBefore(prev);
+    }
+}
+
+function down(event) {
+    var li = $(event).closest('li');
+    var next = li.next();
+    if(next.length) {
+        li.detach().insertAfter(next);
+    }
+}
