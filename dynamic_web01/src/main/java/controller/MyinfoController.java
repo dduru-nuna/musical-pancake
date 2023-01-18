2209dcb41f096ec150b06a717e56c0e9cbacb7b6
syj00062 <syj00062@gmail.com>
2023-01-19 01:23:56 +0900

    주석 설명 추가

--- dynamic_web01/src/main/java/controller/MyinfoController.java
+++ dynamic_web01/src/main/java/controller/MyinfoController.java
@@ -40,16 +40,17 @@ public class MyinfoController extends HttpServlet {
 		String email = req.getParameter("email");
 		
 		UserDTO dto = new UserDTO();
-		dto.setPassword(changePass);
+		dto.setPassword(changePass); //사용자가 입력한 변경할 패스워드로 패스워드 저장
 		dto.setEmail(email);
 		
 		UserService service = new UserService();
-		boolean result = service.update(userData, dto, password);
+		boolean result = service.update(userData, dto, password); 
+		      //   (로그인 사용자의 id/password/email 담긴 객체, id/변경 패스워드/변경 이메일 담긴 객체, 기존 패스워드 문자열) 정보 service.update 에 전달
 		
-		if(result) {
+		if(result) {  //개인정보 수정 되면 /web01/logout 으로 가게함 
 			resp.sendRedirect(req.getContextPath() + "/logout");
 		} else {
-			req.setAttribute("error", "문제 발생");
+			req.setAttribute("error", "개인 정보 수정에 문제 발생");
 			req.getRequestDispatcher("/WEB-INF/view/myinfo.jsp").forward(req, resp);
 		}
 		
--- dynamic_web01/src/main/java/model/service/UserService.java
+++ dynamic_web01/src/main/java/model/service/UserService.java
@@ -40,18 +40,18 @@ public class UserService {
 
 	public boolean update(UserDTO userData, UserDTO dto, String password) {
 		UserDAO dao = new UserDAO();
-		UserDTO data = dao.selectUser(userData);
+		UserDTO data = dao.selectUser(userData);  //로그인 사용자 정보가 담긴 userData를 selectUser 로 조회한 결과를 data 에 저장
 		
 		if(data == null) {
 			dao.close();
 			return false;
 		} else {
-			if(data.getPassword().equals(password)) {
-				dto.setUserId(data.getUserId());
-				if(dto.getPassword().isEmpty()) {
+			if(data.getPassword().equals(password)) {  //로그인 사용자 비밀번호와 입력한 현재 비밀번호 데이터가 같은 경우(개인정보수정을 하려면 현재 비밀번호를 맞게 입력해야 가능하게 함)
+				dto.setUserId(data.getUserId());  //myinfo 서블릿에서 dto에 변경 패스워드와 변경 이메일만 설정했는데 여기서 로그인사용자의 id로 userId 설정
+				if(dto.getPassword().isEmpty()) {  //혹시나 만약 변경 패스워드 입력 안한 경우 기존 패스워드 유지하게 함
 					dto.setPassword(data.getPassword());
 				}
-				int count = dao.update(dto);
+				int count = dao.update(dto); //로그인 사용자 아이디, 변경할 패스워드, 변경할 이메일 값 가진 dto 가지고 update 구문 실행했을 때 제대로 반영되면 1(행 한개) 반환됨
 				if(count == 1) {
 					dao.commit(); dao.close();
 					return true;
