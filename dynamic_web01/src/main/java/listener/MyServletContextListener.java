791477542ae15d6149143d9de650340dfb0fc29e
syj00062 <syj00062@gmail.com>
2023-01-19 21:26:50 +0900

    Servlet 리스너

---
+++ dynamic_web01/src/main/java/listener/MyServletContextListener.java
@@ -0,0 +1,24 @@
+package listener;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class MyServletContextListener implements ServletContextListener {
+
+	@Override  //어플리케이션(서버) 전역적으로 사용할 데이터가 있으면 여기에서 셋팅하면 된다.
+	public void contextInitialized(ServletContextEvent sce) {
+		ServletContextListener.super.contextInitialized(sce);
+		System.out.println("컨텍스트 객체가 생성되었습니다");
+		sce.getServletContext().setAttribute("hello", "안녕하세요");
+	}
+	
+	@Override //어플리케이션(서버) 종료될 때 해야할게 있으면 여기에 셋팅
+	public void contextDestroyed(ServletContextEvent sce) {
+		ServletContextListener.super.contextDestroyed(sce);
+		System.out.println("컨텍스트 객체가 소멸되었습니다");
+	}
+	
+	
+}
---
+++ dynamic_web01/src/main/java/listener/MyServletRequestListener.java
@@ -0,0 +1,23 @@
+package listener;
+
+import jakarta.servlet.ServletRequestEvent;
+import jakarta.servlet.ServletRequestListener;
+import jakarta.servlet.annotation.WebListener;
+import jakarta.servlet.http.HttpServletRequest;
+
+@WebListener 
+public class MyServletRequestListener implements ServletRequestListener {
+	
+	@Override  //요청이 되면 실행
+	public void requestInitialized(ServletRequestEvent sre) {
+		ServletRequestListener.super.requestInitialized(sre);
+		System.out.println("Request 객체가 생성되었습니다.");
+	//	HttpServletRequest req = (HttpServletRequest)sre.getServletRequest(); 상위 객체인 ServletRequest 로 반환하니 만약 HttpServletRequest 로 반환하려면 다운캐스팅 필요
+	}
+	
+	@Override  //응답되면 실행
+	public void requestDestroyed(ServletRequestEvent sre) {
+		ServletRequestListener.super.requestDestroyed(sre);
+		System.out.println("Request 객체가 소멸되었습니다.");
+	}
+}
