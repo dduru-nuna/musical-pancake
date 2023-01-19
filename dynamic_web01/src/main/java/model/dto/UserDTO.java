48dce495c3ae4200ad0c39590690ee519c9c1652
syj00062 <syj00062@gmail.com>
2023-01-19 21:48:33 +0900

    DTO에 httpsessionbinding리스너 추가

--- dynamic_web01/src/main/java/model/dto/UserDTO.java
+++ dynamic_web01/src/main/java/model/dto/UserDTO.java
@@ -1,6 +1,10 @@
 package model.dto;
 
-public class UserDTO {
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSessionBindingEvent;
+import jakarta.servlet.http.HttpSessionBindingListener;
+//userDTO 가 세션에 바인딩(데이터를 붙였을 때) 됐을 때만 리스너 실행-(메세지출력) 
+public class UserDTO implements HttpSessionBindingListener {
 
 	private String userId;
 	private String password;
@@ -27,5 +31,19 @@ public class UserDTO {
 	@Override
 	public String toString() {
 		return "UserDTO [userId=" + userId + ", password=" + password + ", email=" + email + "]";
+	}
+	
+	@Override  //dto데이터 set 할 때
+	public void valueBound(HttpSessionBindingEvent event) {
+		HttpSessionBindingListener.super.valueBound(event);
+		HttpSession session = event.getSession(); //set 됐을 때 세션에 추가할 것이 있으면 여기서 설정
+		session.setAttribute("login", true);
+	}
+	
+	@Override  //dto데이터 remove, invalidate 할 때
+	public void valueUnbound(HttpSessionBindingEvent event) {
+		HttpSessionBindingListener.super.valueUnbound(event);
+		HttpSession session = event.getSession(); //remove,invalidate 됐을 때 세션에 추가할 것이 있으면 여기서 설정
+		session.setAttribute("login", false);		System.out.println("session unbinding: " + event.getName() + " - " + event.getValue());
 	}
 }
