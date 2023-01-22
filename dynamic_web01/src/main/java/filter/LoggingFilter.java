3ebfd45f5e04d49879d2e80456883a5128f37d9c
syj00062 <syj00062@gmail.com>
2023-01-22 23:52:48 +0900

    로깅 필터 설명주석 추가

--- dynamic_web01/src/main/java/filter/LoggingFilter.java
+++ dynamic_web01/src/main/java/filter/LoggingFilter.java
@@ -19,27 +19,28 @@ public class LoggingFilter implements Filter{
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest)req;
+		HttpServletRequest request = (HttpServletRequest)req;  //ServletRequest -> HttpServletRequest 다운캐스팅 필요
 		HttpServletResponse response = (HttpServletResponse)resp;
 		
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd a KK:mm:ss.SSS"); //KK -> 01~12 로 일의 자리를 0 붙여서 표현
 		
 		String path = request.getRequestURI();
-		String method = request.getMethod();
-		String query = request.getQueryString();
+		String method = request.getMethod();  // request 객체에서 get방식, post방식 등의 정보 가져와서 method에 저장
+		String query = request.getQueryString();  //request객체의 uri 에서 ? 뒤의 parameter 정보 가져와서 query에 저장
 		
 		System.out.printf("[%s] - %s: %s\n", df.format(new Date()), method, path);
 		if(query != null) {
-			System.out.println("?" + query);  //url 주소에 나오는 parameter 보여줌(? 이후)
+			System.out.println("?" + query); 
 		}
 		
 		chain.doFilter(req, resp); //다음 필터 호출. 계속 연계하다가 마지막에 있는 필터는 최종적으로 서블릿으로 연계한다.
 		
-		int code = response.getStatus();
+		//response객체에서 응답된 http 상태 코드정보 code에 저장. ex.404:잘못된 요청(페이지없음), 200:정상, 302:리다이렉트, 500:서버오류
+		int code = response.getStatus(); 
 		String redirect = response.getHeader("Location");
 		
 		System.out.printf("[%s] - %s: ", df.format(new Date()), code);
-	// http 상태 코드 ex.404:잘못된 요청(페이지없음), 200:정상, 302:리다이렉트, 500:서버오류
+	
 		switch(code / 100) {
 		case 2:
 			System.out.print("정상"); 
