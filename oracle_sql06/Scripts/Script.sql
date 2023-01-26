c4318eb23680d48a92025af9b4112731e1d08a1f
syj00062 <syj00062@gmail.com>
2023-01-26 21:34:36 +0900

    공지사항 있는 게시판 테이블 생성, 조회

--- oracle_sql06/Scripts/Script.sql
+++ oracle_sql06/Scripts/Script.sql
@@ -70,3 +70,69 @@ SELECT *
  WHERE NUM BETWEEN 4 AND 6;
  
 SELECT COUNT(*) FROM BOOKMARK_T WHERE USERID = 'aaaa';
+
+
+/**
+ * 공지사항이 있는 게시판
+ * btype 이 N 인 경우 공지사항 입니다.
+ * btype 이 B 인 경우 일반 게시글 입니다.
+*/
+CREATE TABLE BOARD_T(
+	id NUMBER PRIMARY KEY
+  , btype VARCHAR2(1) CHECK(btype IN ('N', 'B'))
+  , title VARCHAR2(500) NOT NULL
+  , writer VARCHAR2(20) REFERENCES USER_T(userId)
+  , context VARCHAR2(4000)
+  , createDate DATE DEFAULT(SYSDATE)
+  , updateDate DATE DEFAULT(SYSDATE)
+  , viewCnt NUMBER DEFAULT(0)
+);
+CREATE SEQUENCE BOARD_S;
+
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 1', 'aaaa', '게시글 테스트 중입니다. 1'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 2', 'aaaa', '게시글 테스트 중입니다. 2'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 3', 'aaaa', '게시글 테스트 중입니다. 3'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 4', 'aaaa', '게시글 테스트 중입니다. 4'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'N', '공지글 테스트 1', 'aaaa', '공지글 테스트 중입니다. 1'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'N', '공지글 테스트 2', 'aaaa', '공지글 테스트 중입니다. 2'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'N', '공지글 테스트 3', 'aaaa', '공지글 테스트 중입니다. 3'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 5', 'aaaa', '게시글 테스트 중입니다. 5'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 6', 'aaaa', '게시글 테스트 중입니다. 6'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 7', 'aaaa', '게시글 테스트 중입니다. 7'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'N', '공지글 테스트 4', 'aaaa', '공지글 테스트 중입니다. 4'
+                           , DEFAULT, DEFAULT, DEFAULT);      
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'N', '공지글 테스트 5', 'aaaa', '공지글 테스트 중입니다. 5'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 8', 'aaaa', '게시글 테스트 중입니다. 8'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 9', 'aaaa', '게시글 테스트 중입니다. 9'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 10', 'aaaa', '게시글 테스트 중입니다. 10'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 11', 'aaaa', '게시글 테스트 중입니다. 11'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 12', 'aaaa', '게시글 테스트 중입니다. 12'
+                           , DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO BOARD_T VALUES(BOARD_S.NEXTVAL, 'B', '일반 게시글 테스트 13', 'aaaa', '게시글 테스트 중입니다. 13'
+                           , DEFAULT, DEFAULT, DEFAULT);
+                           
+SELECT * FROM BOARD_T;  
+
+SELECT id, btype, title, writer, createDate, viewCnt
+  FROM(SELECT ROWNUM AS NUM
+            , id, btype, title, writer, createDate, viewCnt
+         FROM (SELECT *
+                 FROM BOARD_T
+                ORDER BY btype DESC, id DESC)
+  )
+ WHERE NUM BETWEEN 1 AND 10;
