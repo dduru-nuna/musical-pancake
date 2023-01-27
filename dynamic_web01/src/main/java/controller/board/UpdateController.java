7aa61e53670cca3e7da5dca0a111fd9e6fb5c952
syj00062 <syj00062@gmail.com>
2023-01-27 21:36:36 +0900

    게시글 수정 작업

--- dynamic_web01/src/main/java/controller/board/UpdateController.java
+++ dynamic_web01/src/main/java/controller/board/UpdateController.java
@@ -1,9 +1,69 @@
 package controller.board;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.dto.BoardDTO;
+import model.dto.UserDTO;
+import model.service.BoardService;
 
 @WebServlet("/board/update")
 public class UpdateController extends HttpServlet {
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String id = req.getParameter("id");
+		BoardDTO dto = new BoardDTO();
+		dto.setId(Integer.parseInt(id));
+		
+		BoardService service = new BoardService();
+		BoardDTO data = service.getData(dto);
+		
+		//여기서도 작성자와 로그인 사용자가 일치할 때만 수정페이지가 보이도록 작업해야함(완료)
+		HttpSession session = req.getSession();
+		UserDTO user = (UserDTO)session.getAttribute("user");
+		if(data.getWriter().equals(user.getUserId())) {
+			req.setAttribute("data", data);
+			req.getRequestDispatcher("/WEB-INF/view/board/update.jsp").forward(req, resp);	
+		} else {
+			resp.sendRedirect(req.getContextPath() + "/error");
+		}
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		UserDTO user = (UserDTO)session.getAttribute("user");
+		
+		String id = req.getParameter("id");
+		String title = req.getParameter("title");
+		String context = req.getParameter("context");
+		
+		BoardDTO dto = new BoardDTO();
+		dto.setId(Integer.parseInt(id));
+		
+		BoardService service = new BoardService();
+		BoardDTO data = service.getData(dto);
+		
+		if(data.getWriter().equals(user.getUserId())) {  //작성자와 로그인 사용자가 같을 때만 수정작업 가능하게
+			data.setTitle(title);
+			data.setContext(context);
+			
+			boolean result = service.updateData(data);
+			if(result) {
+				resp.sendRedirect(req.getContextPath() + "/board/detail?id=" + data.getId());
+			} else {
+				req.getRequestDispatcher("/WEB-INF/view/board/update.jsp").forward(req, resp);
+			}
+		} else {
+			resp.sendRedirect(req.getContextPath() + "/error");
+		}
+		
+	}
 }
--- dynamic_web01/src/main/webapp/WEB-INF/view/board/update.jsp
+++ dynamic_web01/src/main/webapp/WEB-INF/view/board/update.jsp
@@ -1,12 +1,38 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>    
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>게시글 수정 페이지</title>
+<c:url var="staticUrl" value="/static" />
+<link type="text/css" rel="stylesheet" href="${staticUrl }/bs5/css/bootstrap.min.css">
+<script type="text/javascript" src="${staticUrl }/bs5/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-
+	<div>
+		<h1>글 수정 - ${requestScope.data.title }</h1>
+	</div>
+	<div>
+		<c:url var="boardUpdateUrl" value="/board/update" />
+		<form action="${boardUpdateUrl }" method="post">
+			<input type="hidden" name="id" value="${requestScope.data.id }">
+			<div>
+				<label>제목</label>
+				<input type="text" name="title" value="${requestScope.data.title }">
+			</div>
+			<div>
+				<label>내용</label>
+				<textarea name="context">${requestScope.data.context }</textarea>
+			</div>
+			<div>
+				<button type="submit">수정</button>
+				<button type="button" onclick="history.back();">취소</button>
+			</div>
+		</form>
+	</div>
 </body>
 </html>
