2613e59a68126802b18895aea875410d120a0eda
syj00062 <syj00062@gmail.com>
2023-01-27 20:44:32 +0900

    게시글 조회수 작업을 위한 리스너 추가

--- dynamic_web01/src/main/java/listener/MyHttpSessionListener.java
+++ dynamic_web01/src/main/java/listener/MyHttpSessionListener.java
@@ -1,5 +1,7 @@
 package listener;
 
+import java.util.ArrayList;
+
 import jakarta.servlet.annotation.WebListener;
 import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.HttpSessionEvent;
@@ -13,6 +15,7 @@ public class MyHttpSessionListener implements HttpSessionListener {
 		HttpSessionListener.super.sessionCreated(se);
 		HttpSession session = se.getSession();
 		session.setAttribute("login", false); //로그인 안한 경우를 false라고 지정해서 이전에 null인 경우로 따지지 않아도 됨
+		session.setAttribute("boardViewHistory", new ArrayList<Integer>()); //조회 기록이 있는지 확인(id 확인)
 	}
 	
 	@Override
