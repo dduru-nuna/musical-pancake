9eadf417434c263f373e4bc475313bb8f9e37996
syj00062 <syj00062@gmail.com>
2023-01-27 21:32:47 +0900

    게시글 추가 작성

--- dynamic_web01/src/main/java/controller/board/AddController.java
+++ dynamic_web01/src/main/java/controller/board/AddController.java
@@ -1,9 +1,43 @@
 package controller.board;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.dto.BoardDTO;
+import model.dto.UserDTO;
+import model.service.BoardService;
 
 @WebServlet("/board/add")
 public class AddController extends HttpServlet {
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/view/board/add.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession(); //세션에 계정 정보가 있으니 필요
+		String title = req.getParameter("title");
+		String context = req.getParameter("context");
+		
+		BoardDTO dto = new BoardDTO();
+		dto.setTitle(title);
+		dto.setContext(context);
+		dto.setWriter(((UserDTO)session.getAttribute("user")).getUserId());
+		dto.setBtype("B");
+		
+		BoardService service = new BoardService();
+		boolean result = service.add(dto);
+		if(result) {
+			resp.sendRedirect(req.getContextPath() + "/board/detail?id=" + dto.getId());
+		} else {
+			req.getRequestDispatcher("/WEB-INF/view/board/add.jsp").forward(req, resp);
+		}
+	}
 }
--- dynamic_web01/src/main/webapp/WEB-INF/view/board/add.jsp
+++ dynamic_web01/src/main/webapp/WEB-INF/view/board/add.jsp
@@ -1,12 +1,37 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>    
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>게시글 추가 페이지</title>
+<c:url var="staticUrl" value="/static" />
+<link type="text/css" rel="stylesheet" href="${staticUrl }/bs5/css/bootstrap.min.css">
+<script type="text/javascript" src="${staticUrl }/bs5/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-
+	<div>
+		<h1>글 작성</h1>
+	</div>
+	<div>
+		<c:url var="boardAddUrl" value="/board/add" />
+		<form action="${boardAddUrl }" method="post">
+			<div>
+				<label>제목</label>
+				<input type="text" name="title">
+			</div>
+			<div>
+				<label>내용</label>
+				<textarea name="context"></textarea>
+			</div>
+			<div>
+				<button type="submit">저장</button>
+				<button type="button" onclick="history.back();">취소</button> <%--back() : 뒤로가기 (자바스크립트기능) --%>
+			</div>
+		</form>
+	</div>
 </body>
 </html>
