3c917a0ca9db8b6f76a026172386d1e19d3abac5
syj00062 <syj00062@gmail.com>
2023-02-08 21:41:02 +0900

    추천/비추천 비동기로 바꾼 후 로그인 필터 설정 추가

--- dynamic_web01/src/main/java/filter/LoginCheckFilter.java
+++ dynamic_web01/src/main/java/filter/LoginCheckFilter.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebFilter({"/bookmark", "/myinfo", "/bookmark/*", "/board/add", "/board/update", "/board/delete", "/board/clickGood", "/board/clickBad"})
+@WebFilter({"/bookmark", "/myinfo", "/bookmark/*", "/board/add", "/board/update", "/board/delete", "/board/clickGood", "/board/clickBad", "/ajax/recommend"})
 public class LoginCheckFilter implements Filter {
 
 	@Override
@@ -25,7 +25,14 @@ public class LoginCheckFilter implements Filter {
 		if((boolean)session.getAttribute("login")) {
 			chain.doFilter(req, resp);
 		} else {
-			response.sendRedirect(request.getContextPath() + "/login");
+			//비동기는 sendRedirect 로 전환되지 않음. 따라서 uri에 ajax(비동기 처리는 ajax를 항상 사용할 것이다) 포함되면 getWriter로 응답 주소 직접 설정
+			if(request.getRequestURI().contains("ajax")) {
+				//getWriter() -> PrintWriter 객체 가져오기
+				response.getWriter().print("{\"redirect\": \"" + request.getContextPath() + "/login\"}");
+				response.getWriter().flush();
+			} else {
+				response.sendRedirect(request.getContextPath() + "/login");		
+			}
 		}
 
 	}
